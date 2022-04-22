1a163f1e67e7635ce4a4d5d344b735cf36064bc5
Gary Chang <gary.chang@unity3d.com>
2022-04-22 05:45:34 -0400

    Add note about how to use use raycasting with old input manager

--- NavMesh/Assets/Scripts/ClickDestination.cs
+++ NavMesh/Assets/Scripts/ClickDestination.cs
@@ -14,7 +14,19 @@ public class ClickDestination : MonoBehaviour
     void Update()
     {
         // NOTE: Use a key or button that is convenient to you
-        //Keyboard.current.spaceKey.wasPressedThisFrame
+        // Keyboard.current.spaceKey.wasPressedThisFrame
+
+        // NOTE 2: If you wish to use the old Input Manager instead of the new Input System,
+        //         you can use the commented code below and disable the rest
+        // if (Input.GetMouseButtonDown(0))
+        // {
+        //     Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //     RaycastHit hit;
+        //     if (Physics.Raycast(ray, out hit, 100.0f))
+        //     {
+        //         m_Agent.SetDestination(hit.point);
+        //     }
+        // }
 
         if (Mouse.current.leftButton.wasPressedThisFrame)
         {
